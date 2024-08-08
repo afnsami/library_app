3773ef5f1d8d1a006dcb816a3d28502dd9ffbbe4
anaseig <70536934+anaseig@users.noreply.github.com>
2024-08-08 20:25:32 +0600

    Feat: book cards can be added by input, Add: preventDefault()

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-//INPUT ELEMENTS
-const body = document.getElementById("body");
+//ELEMENTS
 const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
 const hasRead = document.getElementById("hasRead");
+const addBookButton = document.getElementById("addBookButton");
+const booksContainer = document.getElementById("booksContainer");
 
 
 
@@ -19,17 +20,42 @@ function Book(title, author, hasRead) {
 
 //adds book to the library
 function addBookToLibrary(title, author, hasRead) {
-
     for (i = 0; i < 10; i++) {
         bookLibrary['obj' + i] = new Book(title, author, hasRead);
     }
-
 }
 
-document.body.onload = addElement;
 
-function addBookCall() {
+//ADDS BOOK TO LIBRARY
+addBookButton.addEventListener('click', function(event) {
     addBookToLibrary(titleInput.value, authorInput.value, hasRead.value);
 
-    alert(bookLibrary.obj1['title']);
-}
+    const newBook = document.createElement('div');
+    newBook.className = "bookCard";
+    
+    //TITLE
+    const titlePara = document.createElement('p');
+    titlePara.textContent = titleInput.value;
+    newBook.append(titlePara);
+    //AUTHOR
+    const authorPara = document.createElement('p');
+    authorPara.textContent = authorInput.value;
+    newBook.append(authorPara);
+    //hasREAD
+    const hasReadButton = document.createElement("button");
+    hasReadButton.textContent = hasRead.value;
+    hasReadButton.className = "interectButton";
+    newBook.append(hasReadButton);
+    //DELETE
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.id = "deleteButton";
+    deleteButton.className = "interectButton";
+    newBook.append(deleteButton);
+
+
+
+
+    booksContainer.appendChild(newBook);
+    event.preventDefault();
+});
